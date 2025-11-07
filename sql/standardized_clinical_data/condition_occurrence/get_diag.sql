e249a7d2884b617ddda8b95de639d8452b3cf14f
Léodoric AHOUANSE <leodoric.ahouanse@chu-bordeaux.fr>
2025-11-07 18:31:58 +0100

    hackathon diag

---
+++ sql/standardized_clinical_data/condition_occurrence/get_diag.sql
@@ -0,0 +1,56 @@
+WITH rss_corrige AS (
+    SELECT
+        idpat,
+        sej,
+        CASE WHEN rssdentree <= rssdsortie THEN rssdentree ELSE rssdsortie END AS date_debut_corrigee,
+        CASE WHEN rssdentree <= rssdsortie THEN rssdsortie ELSE rssdentree END AS date_fin_corrigee
+    from
+    	pmsi.hackathon_rss
+    where
+    	idpat || '_' || sej != 'IDPAT344036' || '_' || 'SEJ0165083'
+),
+borne_venue AS (
+    SELECT
+        idpat,
+        sej,
+        MIN(date_debut_corrigee) AS date_debut_venue,
+        MAX(date_fin_corrigee) AS date_fin_venue
+    from
+    	rss_corrige
+    GROUP by
+    	idpat, sej
+),
+um_rang AS (
+    SELECT
+        pmsi.hackathon_rum.*,
+        ROW_NUMBER() OVER (PARTITION BY sej ORDER BY rumdentree ASC) AS rang
+    from
+    	pmsi.hackathon_rum
+),
+first_um as (
+	select
+		um_rang.*
+	from
+		um_rang
+	where
+		um_rang.rang = 1
+),
+diag as (
+select hd.idpat, hd.sej,hd.rsstypediagnostic,hd.diag
+from pmsi.hackathon_diag hd
+)
+SELECT
+    b.idpat,
+    b.sej,
+    b.date_debut_venue,
+    b.date_fin_venue,
+    um.um as um_entree,
+    um.ummodehospitalisation as um_mode_hospitalisation
+from
+	borne_venue b,
+	first_um um,
+	diag d
+where
+	b.sej = um.sej and b.sej = d.sej
+ORDER by
+	b.idpat, b.sej;
